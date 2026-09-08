cbf76bd2c1916b6d04c7d1b7d47677aae9cbe2bf
Shubhambither <bithershubham@gmail.com>
2026-09-08 23:29:58 +0530

    This is an updated version of calculator.py

--- 2.Calculator.py
+++ 2.Calculator.py
@@ -15,10 +15,15 @@
 # else:
 #     print(f"Choose a valid operator. You choosed {op}. Choose from = - * / %")
 # print(a,op,b)
-a = input("Enter first operand: ")
-op = input("Enter operator: ")
-b = input("Enter second operand: ")
+# -------------------------------------------------------------------------------
+# a = input("Enter first operand: ")
+# op = input("Enter operator: ")
+# b = input("Enter second operand: ")
 
-print(eval(a + op + b)) 
+# print(eval(a + op + b)) 
+#eval works on input taken as string not as input
+# -------------------------------------------------------------------------------
+expression = input("Enter expression: ")
 
+print(eval(expression))
     
